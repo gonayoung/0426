05b313f93ea476773a5f5635176888e6fc23628b
​gonayoung <sks0716ek@gmail.com>
2025-04-27 21:22:40 +0900

    main commit

--- js/event_area.js
+++ js/event_area.js
@@ -1,36 +1,16 @@
-$(function(){
-    $.getJSON("json/book_events.json", function(data){
-        if(data.length > 0){
-            for(var i in data){
-                let html = `
-                    <li>
-                        <img src="${data[i].img}" alt="${data[i].title}">
-                    </li>
-                `;
-                $(".event_slide_wrap").append(html);
-            }
+$(function () {
+    $.getJSON("json/event_area.json", function (data) {
+      if (data.length > 0) {
+        for (let i in data) {
+          let html = `
+            <li>
+              <img src="img/${data[i].img}" alt="${data[i].title}">
+              <p class="event_title">${data[i].title}</p>
+              <p class="event_date">${data[i].date}</p>
+            </li>
+          `;
+          $(".event_list").append(html);
         }
+      }
     });
-
-    let currentIndex = 0;
-
-    $(".event_next").click(function(){
-        let slideCount = $(".event_slide_wrap li").length;
-        if(currentIndex < slideCount - 1){
-            currentIndex++;
-        } else {
-            currentIndex = 0;
-        }
-        $(".event_slide_wrap").css("margin-left", -(815 * currentIndex) + "px");
-    });
-
-    $(".event_prev").click(function(){
-        let slideCount = $(".event_slide_wrap li").length;
-        if(currentIndex > 0){
-            currentIndex--;
-        } else {
-            currentIndex = slideCount - 1;
-        }
-        $(".event_slide_wrap").css("margin-left", -(815 * currentIndex) + "px");
-    });
-});
+  });
